c251e2dc99d858f81e8dd9f20f46257bf250a317
ArshM17 <arshmaknojia5@gmail.com>
2023-07-20 17:07:51 +0530

    Initial setup

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const path = require("path");
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client", "home.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
