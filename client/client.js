1af56f3eaed070242f2592d9e4e007660bd443f4
ArshM17 <arshmaknojia5@gmail.com>
2023-07-24 17:01:34 +0530

    Handle connect and disconnect

--- client/client.js
+++ client/client.js
@@ -1,1 +1,12 @@
 const socket = io();
+var PLAYERS = [];
+
+socket.on("connect", () => {
+  console.log(socket.id);
+  socket.emit("new-player", socket.id);
+});
+
+socket.on("update-players", (list) => {
+  PLAYERS = list;
+  console.log(PLAYERS);
+});
--- server/server.js
+++ server/server.js
@@ -6,14 +6,33 @@ const path = require("path");
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const PLAYERS = [];
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client", "home.html"));
 });
 
+app.use(express.static(path.join(__dirname, "../client")));
+
 io.on("connection", (socket) => {
-  console.log(socket.id);
+  console.log(`new player: ${socket.id}`);
+  socket.on("new-player", (id) => {
+    PLAYERS.push({ id: id, progress: 0 });
+    io.emit("update-players", PLAYERS);
+  });
+  socket.on("disconnect", () => {
+    var idx;
+    for (var i = 0; i < PLAYERS.length; i++) {
+      if (PLAYERS[i].id == socket.id) {
+        idx = i;
+        break;
+      }
+    }
+    PLAYERS.splice(idx, 1);
+    io.emit("update-players", PLAYERS);
+  });
 });
 
 server.listen(3000, () => {
-  console.log("listening on *:3000");
+  console.log("Server Started");
 });
